546c231498dd5f8e88388c67fcaeecffffaa2752
Riviane Rose <107583014+rivianerose@users.noreply.github.com>
2023-03-13 18:37:37 -0300

    Lampada atualizada
    lampada

---
+++ lampada.js
@@ -0,0 +1,11 @@
+function acender() {
+document.querySelector('#off').src = './imagens/lampadaacesa.jpg'
+}
+
+function apagar() {
+    document.querySelector('#off').src = './imagens/lampadaapagada.jpg'
+    }
+
+    function quebrar() {
+        document.querySelector('#off').src = './imagens/lampadaquebrada.jpg'
+        }
